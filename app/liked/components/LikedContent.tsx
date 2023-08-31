8bc488185cc96d605d4c2b863db990cfc5685d60
sarthakbakshii <sarthakbakshi14@gmail.com>
2023-09-01 00:24:06 +0530

    created liked songs setcion

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+
+interface Props {
+  songs: Song[];
+}
+
+const LikedContent: React.FC<Props> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  // if no user return to home
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, router, user]);
+
+  // if no song
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No liked songs.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+        {
+            songs.map((song) => (
+                <div key={song.id} className="flex items-center gap-x-4 w-full">
+                    <div className="flex-1">
+                        <MediaItem
+                          onClick={() =>{}}
+                          data={song}
+                         />
+                    </div>
+                    <LikeButton songId={song.id} />
+                </div>
+            ))
+        }
+    </div>
+  )
+};
+
+export default LikedContent;
